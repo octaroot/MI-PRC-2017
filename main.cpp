005634c3f478ec2c9ed7feeeb3e7a2131a2c5dd5
Martin Černáč <me@martincernac.cz>
2017-03-11 16:17:00 +0100

    Add the whole Canny Edge Detector algo (via Rosettacode)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
-#define KERNEL_SIZE	159
+#define KERNEL_SIZE            159
+#define PIXEL_SIZE                3
+#define MAX_BRIGHTNESS            255
 
 // via http://stackoverflow.com/questions/9296059/read-pixel-value-in-bmp-file
 unsigned char *readBMP(const char *filename, int *width, int *height)
@@ -31,7 +34,7 @@ unsigned char *readBMP(const char *filename, int *width, int *height)
 	return data;
 }
 
-void writeBMP(const char * filename, unsigned char * data, int width, int height)
+void writeBMP(const char *filename, unsigned char *data, int width, int height)
 {
 	int size = 3 * width * height;
 
@@ -58,42 +61,160 @@ void writeBMP(const char * filename, unsigned char * data, int width, int height
 	fclose(f);
 }
 
-int main()
-{
+void convolution(
+		unsigned char *in,
+		unsigned char *out,
+		const int width,
+		const int height,
+		const char *kernel,
+		const int kernelSize,
+		const int kernelSum
+) {
+	const int offset = (kernelSize - 1) / 2;
+
+	for (int x = offset; x < width - offset - 1; ++x) {
+		for (int y = offset; y < height - offset - 1; ++y) {
+			int newTotal = 0;
+			for (int i = 0; i < kernelSize; ++i) {
+				for (int j = 0; j < kernelSize; ++j) {
+					newTotal += in[(y + j - offset) * height + x + i - offset] * kernel[j * kernelSize + i];
+				}
+			}
+			out[y * height + x] = (unsigned char) (newTotal / kernelSum);
+		}
+	}
+}
+
+void convolutionWide(
+		unsigned char *in,
+		int *out,
+		const int width,
+		const int height,
+		const char *kernel,
+		const int kernelSize
+) {
+	const int offset = (kernelSize - 1) / 2;
+
+	for (int x = offset; x < width - offset - 1; ++x) {
+		for (int y = offset; y < height - offset - 1; ++y) {
+			int newTotal = 0;
+			for (int i = 0; i < kernelSize; ++i) {
+				for (int j = 0; j < kernelSize; ++j) {
+					newTotal += in[(y + j - offset) * height + x + i - offset] * kernel[j * kernelSize + i];
+				}
+			}
+			out[y * height + x] = newTotal;
+		}
+	}
+}
+
+int main() {
 	int width, height;
-	unsigned char *image = readBMP("/home/martin/Projects/mi-prc-cannyedge/data/lena.bmp", &width, &height);
-	unsigned char *aux = new unsigned char[width * height * 3];
+	unsigned char *image = readBMP("/home/martin/Projects/cpp/MI-PRC-CannyEdge/data/lena.bmp", &width, &height);
+	unsigned char *gauss = new unsigned char[width * height];
+	int *gradientX = new int[width * height];
+	int *gradientY = new int[width * height];
+	double *gradients = new double[width * height];
+	double *nonMaxSupp = new double[width * height];
 	//The color of pixel (i, j) is stored at data[j * width + i], data[j * width + i + 1] and data[j * width + i + 2].
 
-	memcpy(aux, image, width * height * 3);
+	int tmin = 40, tmax = 60;
+
+	memcpy(gauss, image, width * height);
 
 	//step 1 - gauss, |kernel|=5, sum=159
-	const char kernel[5][5]{{2, 4,  5,  4,  2},
-							{4, 9,  12, 9,  4},
-							{5, 12, 15, 12, 5},
-							{4, 9,  12, 9,  4},
-							{2, 4,  5,  4,  2},};
+	const char kernel[]{2, 4, 5, 4, 2,
+						4, 9, 12, 9, 4,
+						5, 12, 15, 12, 5,
+						4, 9, 12, 9, 4,
+						2, 4, 5, 4, 2};
+	convolution(image, gauss, width, height, kernel, 5, KERNEL_SIZE);
 
-	for (int x = 2; x < width - 3; ++x)
-	{
-		for (int y = 2; y < height - 3; ++y)
-		{
-			for (int color = 0; color < 3; ++color)
-			{
-				int newTotal = 0;
-				for (int i = 0; i < 5; ++i)
-				{
-					for (int j = 0; j < 5; ++j)
-					{
-						newTotal += image[(y + j - 2) * (3*height) + 3*(x + i - 2) + color] * kernel[i][j];
-					}
-				}
-				aux[y * (3*height) + 3*x + color] = (unsigned char) (newTotal / KERNEL_SIZE);
-			}
+	//step 2 - intensity gradient (Sobel)
+	const char Gx[] = {-1, 0, 1,
+					   -2, 0, 2,
+					   -1, 0, 1};
+
+	convolutionWide(gauss, gradientX, width, height, Gx, 3);
+
+	const char Gy[] = {1, 2, 1,
+					   0, 0, 0,
+					   -1, -2, -1};
+
+	convolutionWide(gauss, gradientY, width, height, Gy, 3);
+
+	//step 3 - hystersis
+	for (int i = 1; i < width - 1; i++) {
+		for (int j = 1; j < height - 1; j++) {
+			const int c = width * j + i;
+			gradients[c] = (float) hypot(gradientX[c], gradientY[c]);
 		}
 	}
 
+	// Non-maximum suppression, straightforward implementation.
+	for (int i = 1; i < width - 1; i++) {
+		for (int j = 1; j < height - 1; j++) {
+			const int c = (width * j) + (i);
+			const int nn = c - (width);
+			const int ss = c + (width);
+			const int ww = c + 1;
+			const int ee = c - 1;
+			const int nw = nn + 1;
+			const int ne = nn - 1;
+			const int sw = ss + 1;
+			const int se = ss - 1;
 
-	writeBMP("/tmp/copy.bmp", aux, width, height);
+			const float dir = (float) (fmod(atan2(gradientY[c], gradientX[c]) + M_PI, M_PI) / M_PI) * 8;
+
+			if (((dir <= 1 || dir > 7) && gradients[c] > gradients[ee] &&
+				 gradients[c] > gradients[ww]) || // 0 deg
+				((dir > 1 && dir <= 3) && gradients[c] > gradients[nw] &&
+				 gradients[c] > gradients[se]) || // 45 deg
+				((dir > 3 && dir <= 5) && gradients[c] > gradients[nn] &&
+				 gradients[c] > gradients[ss]) || // 90 deg
+				((dir > 5 && dir <= 7) && gradients[c] > gradients[ne] &&
+				 gradients[c] > gradients[sw]))   // 135 deg
+				nonMaxSupp[c] = gradients[c];
+			else
+				nonMaxSupp[c] = 0;
+		}
+	}
+
+	int *edges = new int[width * height];
+	unsigned char *out = new unsigned char[width * height];
+
+	// Tracing edges with hysteresis
+	for (int j = 1; j < height - 1; j++) {
+		for (int i = 1; i < width - 1; i++) {
+			const int c = (width * j) + (i);
+			if (nonMaxSupp[c] >= tmax && out[c] == 0) { // trace edges
+				out[c] = 255;
+				int nedges = 1;
+				edges[0] = c;
+
+				do {
+					const int t = edges[--nedges];
+
+					int nbs[8]; // neighbours
+					nbs[0] = t - (width);     // nn
+					nbs[1] = t + (width);     // ss
+					nbs[2] = t + 1;      // ww
+					nbs[3] = t - 1;      // ee
+					nbs[4] = nbs[0] + 1; // nw
+					nbs[5] = nbs[0] - 1; // ne
+					nbs[6] = nbs[1] + 1; // sw
+					nbs[7] = nbs[1] - 1; // se
+
+					for (int k = 0; k < 8; k++)
+						if (nonMaxSupp[nbs[k]] >= tmin && out[nbs[k]] == 0) {
+							out[nbs[k]] = 255;
+							edges[nedges++] = nbs[k];
+						}
+				} while (nedges > 0);
+			}
+		}
+	}
+	writeBMP("/tmp/copy.bmp", out, width, height);
+
 	return 0;
 }
